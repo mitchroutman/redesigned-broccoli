f3fd6500a892fac889921a4cdc73d3ffc4d98dfb
Vy Nguyen <vy1872@gmail.com>
2022-02-02 13:11:04 -0600

    got autocomplete to work

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,12 +1,8 @@
 //afe8f8ce140548eaaabaa3febe07b051
-///Yujen
+//0d6b19fe0c2b4f04a50900e6cfded5f0/Yujen
 
 spoonacularKey = "62e06ed2bdb843f98dcd0f5d825b6103";
 
-generateRandomRecipeURL = "https://api.spoonacular.com/recipes/random?number=1&apiKey=" + spoonacularKey;
-searchRecipe = "https://api.spoonacular.com/recipes/autocomplete?number=1&apiKey=" + spoonacularKey;
-
-//jQuery Autocomplete
 function randomRecipe() {
     generateRandomRecipeURL = "https://api.spoonacular.com/recipes/random?number=1&apiKey=" + spoonacularKey;
 
@@ -21,29 +17,19 @@ function randomRecipe() {
             var dishImage = data.recipes[0].image                                                       //url to image of dish
 
             //changes header of dish name and steps
-            $("#currentDish").append("<h2>" + dishName + "</h2><img src=\"" + image +"\">");
-            $("#ing").append("<ul id=\"ingredients\">");
-            $("#progress").append("</ul><ol id=\"steps\"></ol>");
             $("#currentDish").append("<h2>" + dishName + "</h2 id=\"dishName\"><img id=\"dishImage\" src=\"" + dishImage + "\"><ul id=\"ingredients\"></ul><ol id=\"steps\"></ol>");
             $("#ingredients").append("<h3>Ingredients</h3>");
             $("#steps").append("<h3>Steps</h3>");
 
             //make li element for ingredients
-            for(let i = 0; i < ingredientList.length; i++){
-                $("#ingredients").append(
-            // "<img src=\"" + ingredientList[i].image + "\">
-            "<li>"  + ingredientList[i].name + "aount:" 
-                +ingredientList[i].measures.us.amount + " " 
-                + ingredientList[i].measures.us.unitLong +"</li>")
             for (let i = 0; i < ingredientList.length; i++) {
                 $("#ingredients").append("<li>" + ingredientList[i].name + "</li>")
             }
-            console.log(ingredientList)
 
             //make li elements for the steps
-            for (let j = 0; j < instructions.length; j++) {
-                var currentStep = j + 1;
-                var instructionsString = instructions[j].step;
+            for (var i = 0; i < instructions.length; i++) {
+                var currentStep = i + 1;
+                var instructionsString = instructions[i].step;
                 $("#steps").append("<li>" + currentStep + ". " + instructionsString + "</li>")
                 currentStep++;
             }
@@ -68,8 +54,12 @@ function searchByIngredient() {
             return response.json();
         })
         .then(function (data) {
-            $("searchBar").autocomplete({
-                source: data
+            var searchData = [];
+            for(var i = 0; i < data.length; i++){
+                searchData[i] = data[i].title
+            }
+            $("#searchBar").autocomplete({
+                source: searchData
             })
         })
 }
@@ -83,3 +73,4 @@ $("#generateRandomRecipe").click(function (event) {
     randomRecipe();
 })
 
+searchByIngredient();
