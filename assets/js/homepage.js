24622b3c5df89fc3d1f9befbdc951a244bd5a0dd
Mitchell Routman <mitchell.routman@gmail.com>
2022-02-02 14:01:07 -0500

    updates to HTML for random drink appending, partially working JS

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,17 +1,20 @@
 //afe8f8ce140548eaaabaa3febe07b051
-///Yujen
+//0d6b19fe0c2b4f04a50900e6cfded5f0//Yujen
 
 spoonacularKey = "";
-cocktailsKey = "";
+cocktailsKey = "74f2d339b2msh8cf0e09097065dep10d3f8jsne564c60a5f87";
 
 generateRandomRecipeURL = "https://api.spoonacular.com/recipes/random?number=1&apiKey=" + spoonacularKey;
 searchRecipe = "https://api.spoonacular.com/recipes/autocomplete?number=1&apiKey=" + spoonacularKey;
 
 searchDrink = "https://cocktails3.p.rapidapi.com/random" + cocktailsKey;
 //jQuery Autocomplete
-function randomRecipe() {
-    generateRandomRecipeURL = "https://api.spoonacular.com/recipes/random?number=1&apiKey=" + spoonacularKey;
 
+//
+
+var searchButton = document.getElementById('search-button');
+
+function randomRecipe() {
     fetch(generateRandomRecipeURL)
         .then(function (response) {
             return response.json();
@@ -20,130 +23,78 @@ function randomRecipe() {
             var dishName = data.recipes[0].title;                                                   //name of dish
             var ingredientList = data.recipes[0].extendedIngredients;                               //array ingredients
             var instructions = data.recipes[0].analyzedInstructions[0].steps;                       //array of ingredients
-            var dishImage = data.recipes[0].image                                                       //url to image of dish
+            var image = data.recipes[0].image
 
             //changes header of dish name and steps
-            $("#currentDish").append("<h2>" + dishName + "</h2><img src=\"" + image +"\">");
-            $("#ing").append("<ul id=\"ingredients\">");
-            $("#progress").append("</ul><ol id=\"steps\"></ol>");
-            $("#currentDish").append("<h2>" + dishName + "</h2 id=\"dishName\"><img id=\"dishImage\" src=\"" + dishImage + "\"><ul id=\"ingredients\"></ul><ol id=\"steps\"></ol>");
+            $("#currentDish").append("<h2>" + dishName + "</h2><img src=\"" + image +"\"><ul id=\"ingredients\"></ul><ol id=\"steps\"></ol>");
             $("#ingredients").append("<h3>Ingredients</h3>");
             $("#steps").append("<h3>Steps</h3>");
 
             //make li element for ingredients
             for(let i = 0; i < ingredientList.length; i++){
-                $("#ingredients").append(
-            // "<img src=\"" + ingredientList[i].image + "\">
-            "<li>"  + ingredientList[i].name + "aount:" 
-                +ingredientList[i].measures.us.amount + " " 
-                + ingredientList[i].measures.us.unitLong +"</li>")
-            for (let i = 0; i < ingredientList.length; i++) {
                 $("#ingredients").append("<li>" + ingredientList[i].name + "</li>")
             }
-            console.log(ingredientList)
 
             //make li elements for the steps
-            for (let j = 0; j < instructions.length; j++) {
+            for(let j = 0; j < instructions.length; j++){
                 var currentStep = j + 1;
                 var instructionsString = instructions[j].step;
-                $("#steps").append("<li>" + currentStep + ". " + instructionsString + "</li>")
+                $("#steps").append("<li>"+ currentStep + ". " + instructionsString + "</li>")
                 currentStep++;
             }
         })
 }
 
-function removeCurrentRecipe() {
-    $("#dishName").remove();
-    $("#dishImage").remove();
+function removeCurrentRecipe(){
     $("#ingredients").remove();
     $("#steps").remove();
 }
 
-function searchByIngredient() {
-    var userInput = $("#searchBar").val();
-
-    searchByIngredientURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + userInput + "&numbers=5&apiKey=" + spoonacularKey;
-
-    fetch(searchByIngredientURL)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data)
-        })
-}
-
-$("#searchBtn").click(function () {
-    searchByIngredient();
-})
-
-$("#generateRandomRecipe").click(function (event) {
+$("#generateRandomRecipe").click(function(event){
     removeCurrentRecipe();
     randomRecipe();
 })
 
-<<<<<<< HEAD
 
 // searchIngredientURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + ingredient + "&numbers=5&apiKey=" + spoonacularKey
 
 // Search random Cocktails
 
-// function randomDrink() {
-//     fetch(searchDrink)
-//         .then(function (response) {
-//             console.log(response);
-//             return response.json();
-//         })
-//         .then(function(data) {
-//             var drinkName = data.body[1].name;
-//             var drinkIngredients = data.body[0].ingredients;
-            
-//                        //changes header of dish name and steps
-//                        $("#currentDrink").append("<h2>" + drinkName + "</h2><ul id=\"drinkIngredients\"></ul><ol id=\"drinkSteps\"></ol>");
-//                        $("#drinkIngredients").append("<h3>Ingredients</h3>");
-//                        $("#drinSteps").append("<h3>Steps</h3>");
-           
-//                        //make li element for ingredients
-//                        for(let c = 0; c < ingredientList.length; i++){
-//                            $("#drinkIngredients").append("<li>" + drinkIngredients[i].name + "</li>")
-//                        }
-           
-//                        //make li elements for the steps
-//                        for(let k = 0; k < instructions.length; j++){
-//                            var currentStep = k + 1;
-//                            var instructionsString = instructions[j].step;
-//                            $("#drinkSteps").append("<li>"+ currentStep + ". " + instructionsString + "</li>")
-//                            currentStep++;
-//                        }
-//                    })
-//            };
+//function randomDrink() {
+    fetch("https://cocktails3.p.rapidapi.com/random", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "cocktails3.p.rapidapi.com",
+            "x-rapidapi-key": "74f2d339b2msh8cf0e09097065dep10d3f8jsne564c60a5f87"
+        }
+    })
+    .then(response => {
+        console.log(response);
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data.body[0]);
+        var drinkName = data.body[0].name;
+        var drinkIngredients = data.body[0].ingredients;
+        
+        //Change drink name and creates sections like ingredients
+        $("#currentDrink").append("<h2>" + drinkName + "</h2> <ul id=\"drinkIngredients\"></ul>");
+        //$("#drinkIngredients").append("<h3>Ingredients</h3>");
+        //$("#drinkSteps").append("<h3>Steps</h3>");
 
+        //li elements for drinks
+        for(let i = 0; i < drinkIngredients.length; i++){
+            $("#ingredients").append("<li>" + drinkIngredients[i].ingredientList + "</li>")
+        }
+    });
+//};
 
-// function randomDrink() {
- fetch("https://cocktails3.p.rapidapi.com/random", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "cocktails3.p.rapidapi.com",
-		"x-rapidapi-key": "74f2d339b2msh8cf0e09097065dep10d3f8jsne564c60a5f87"
-	}
-})
-.then(response => {
-    console.log(response);
-    return response.json();
-})
-.then(function (data) {
-    console.log(data.body[0]);
-    var drinkName = data.body[0].name;
-    var drinkIngredients = data.body[0].ingredients;
-    
-    $("currentDrink").append("<h2>" + drinkName + "</h2>ul id=\"ingredients\"></ul><ol id=\"steps\"></ol>");
-    $("#drinkIngredients").append("<h3>Ingredients</h3>");
-    $("#drinkSteps").append("<h3>Steps</h3>");
-});
+// function removeCurrentDrink(){
+//     $("#drinkIngredients").remove();
+//     $("#drinkSteps").remove();
+// }
 
 // $("#generateRandomDrink").click(function(event){
 //     removeCurrentDrink();
 //     randomDrink();
 // })
-=======
->>>>>>> d8c3960dd79c89965bd4c6570601718eacbc336f
