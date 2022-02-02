877863501a138f96938d4a2249dc6ee0c24845a8
Vy Nguyen <vy1872@gmail.com>
2022-02-02 09:17:47 -0600

    added a h2 for the random recipe

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,10 +1,7 @@
 //afe8f8ce140548eaaabaa3febe07b051
 
-<<<<<<< HEAD
 spoonacularKey = "62e06ed2bdb843f98dcd0f5d825b6103";
-=======
-spoonacularKey = "23d3f98f86ee4a9f96d79a08b3d29065";
->>>>>>> 458bb344d6bb36312658be2226edc49d6364d7e2
+
 generateRandomRecipeURL = "https://api.spoonacular.com/recipes/random?number=1&apiKey=" + spoonacularKey;
 searchRecipe = "https://api.spoonacular.com/recipes/autocomplete?number=1&apiKey=" + spoonacularKey;
 
@@ -25,19 +22,16 @@ function randomRecipe() {
             var instructions = data.recipes[0].analyzedInstructions[0].steps;                       //array of ingredients
 
             //changes header of dish name and steps
-            $("#currentDish").append("<ul id=\"ingredients\"></ul><ol id=\"steps\"></ol>");
-            $("#ingredients").append("<h2>" + dishName + "</h2>");
-            $("#steps").append("<h2>Steps</h2>");
+            $("#currentDish").append("<h2>" + dishName + "</h2><ul id=\"ingredients\"></ul><ol id=\"steps\"></ol>");
+            $("#ingredients").append("<h3>Ingredients</h3>");
+            $("#steps").append("<h3>Steps</h3>");
 
             //make li element for ingredients
             for(let i = 0; i < ingredientList.length; i++){
                 $("#ingredients").append("<li>" + ingredientList[i].name + "</li>")
             }
 
-<<<<<<< HEAD
             //make li elements for the steps
-=======
->>>>>>> 33abb4343f56455fd039176318b7756fa953eb0d
             for(let j = 0; j < instructions.length; j++){
                 var currentStep = j + 1;
                 var instructionsString = instructions[j].step;
@@ -47,7 +41,6 @@ function randomRecipe() {
         })
 }
 
-<<<<<<< HEAD
 function removeCurrentRecipe(){
     $("#ingredients").remove();
     $("#steps").remove();
@@ -58,23 +51,4 @@ $("#generateRandomRecipe").click(function(event){
     randomRecipe();
 })
 
-=======
-
-function renderByIngredient() {
-    console.log("Clicked Search")
-    fetch(generateRandomRecipeURL)
-    .then(function (response) {
-        //console.log(response)
-        return response.json();
-    })
-    .then(function (data) {
-        var dishName = data.recipes[0].title;
-        var ingredientList = data.recipe[0].extendedIngredients;
-        var instructions = data.recipe[0].analyzedInstructions[0].steps;
-    })
-}
-
-searchButton.addEventListener('click', renderByIngredient);
-renderRecipe();
->>>>>>> 458bb344d6bb36312658be2226edc49d6364d7e2
 // searchIngredientURL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + ingredient + "&numbers=5&apiKey=" + spoonacularKey
