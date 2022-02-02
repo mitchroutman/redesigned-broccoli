f7fc9ca88b2f5e440fb9de187879522b69a87e23
Yujen Chen <yujengogo@gmail.com>
2022-02-02 12:57:34 -0500

    adding ingredient amount and units

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,7 @@
 //afe8f8ce140548eaaabaa3febe07b051
 //0d6b19fe0c2b4f04a50900e6cfded5f0//Yujen
 
-spoonacularKey = "";
+spoonacularKey = "0d6b19fe0c2b4f04a50900e6cfded5f0";
 
 generateRandomRecipeURL = "https://api.spoonacular.com/recipes/random?number=1&apiKey=" + spoonacularKey;
 searchRecipe = "https://api.spoonacular.com/recipes/autocomplete?number=1&apiKey=" + spoonacularKey;
@@ -24,14 +24,21 @@ function randomRecipe() {
             var image = data.recipes[0].image
 
             //changes header of dish name and steps
-            $("#currentDish").append("<h2>" + dishName + "</h2><img src=\"" + image +"\"><ul id=\"ingredients\"></ul><ol id=\"steps\"></ol>");
+            $("#currentDish").append("<h2>" + dishName + "</h2><img src=\"" + image +"\">");
+            $("#ing").append("<ul id=\"ingredients\">");
+            $("#progress").append("</ul><ol id=\"steps\"></ol>");
             $("#ingredients").append("<h3>Ingredients</h3>");
             $("#steps").append("<h3>Steps</h3>");
 
             //make li element for ingredients
             for(let i = 0; i < ingredientList.length; i++){
-                $("#ingredients").append("<li>" + ingredientList[i].name + "</li>")
+                $("#ingredients").append(
+            // "<img src=\"" + ingredientList[i].image + "\">
+            "<li>"  + ingredientList[i].name + "aount:" 
+                +ingredientList[i].measures.us.amount + " " 
+                + ingredientList[i].measures.us.unitLong +"</li>")
             }
+            console.log(ingredientList)
 
             //make li elements for the steps
             for(let j = 0; j < instructions.length; j++){
