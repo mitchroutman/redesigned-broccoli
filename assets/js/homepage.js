6a4d2386d7c633c15ddf59e3f8b1159ebe84a48f
Vy Nguyen <vy1872@gmail.com>
2022-02-04 09:38:45 -0600

    got autocomplete fully functioning

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,14 +1,7 @@
 //afe8f8ce140548eaaabaa3febe07b051
 //0d6b19fe0c2b4f04a50900e6cfded5f0/Yujen
-<<<<<<< HEAD
 //74f2d339b2msh8cf0e09097065dep10d3f8jsne564c60a5f87
 spoonacularKey = "62e06ed2bdb843f98dcd0f5d825b6103";
-=======
-spoonacularKey = "23d3f98f86ee4a9f96d79a08b3d29065";
-cocktailsKey = "";
-searchDrink = "https://cocktails3.p.rapidapi.com/random" + cocktailsKey;
-
->>>>>>> 7991e6ba237f79c10834c229241e8cbdf3bd3dcd
 
 var searchButton = document.getElementById('search-button');
 
@@ -18,87 +11,33 @@ function randomRecipe() {
 
     fetch(generateRandomRecipeURL)
         .then(function (response) {
-            console.log(response)
             return response.json();
         })
-<<<<<<< HEAD
         .then(function (info) {
-            console.log(info)
+            insertRecipe(info.recipes[0]);
+        })
+}
+
+function findRecipe(recipe){
+    var userInput = recipe;
+
+    var searchRecipeURL = "https://api.spoonacular.com/recipes/" + userInput + "/information?apiKey=" + spoonacularKey;
+    fetch(searchRecipeURL)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(info){
+            removeCurrentRecipe();
             insertRecipe(info);
-=======
-        .then(function (data) {
-            var dishName = data.recipes[0].title;                                                   //name of dish
-            var ingredientList = data.recipes[0].extendedIngredients;                               //array ingredients
-            var instructions = data.recipes[0].analyzedInstructions[0].steps;                       //array of ingredients
-            var dishImage = data.recipes[0].image                                                   //array of images
-            var ingAndPicDiv = document.createElement("div")
-            ingAndPicDiv.setAttribute("id", "ingAndPic")
-            var picAndNameDiv = document.createElement("div")
-            picAndNameDiv.setAttribute("id", "picAndName")
-            var dishNameH2 = document.createElement("h2")
-            dishNameH2.setAttribute("id", "dishName")
-            var dishImageImg = document.createElement("img")
-            dishImageImg.setAttribute("id", "dishImage")
-            var ingredientsUl = document.createElement("ul")
-            ingredientsUl.setAttribute("id", "ingredients")
-            var stepsOl = document.createElement("ol")
-            stepsOl.setAttribute("id", "steps")
-            var currentDishId = document.querySelector("#currentDish")
-
-            // console.log(dishName)
-            dishNameH2.textContent = dishName
-            dishImageImg.src = dishImage
-            picAndNameDiv.append(dishNameH2, dishImageImg)
-
-
-            //changes header of dish name and steps
-            // $("#currentDish").append("<h2>" + dishName + "</h2 id=\"dishName\"><img id=\"dishImage\" src=\"" + dishImage + "\"><ul id=\"ingredients\"></ul><ol id=\"steps\"></ol>");
-            // $("#ingredients").append("<h3>Ingredients</h3>");
-            // $("#steps").append("<h3>Steps</h3>");
-
-            //make li element for ingredients
-            // console.log(ingredientList)
-
-            // ingredientlist
-            for (let i = 0; i < ingredientList.length; i++) {
-                var ingredientString = ingredientList[i].original
-                var ingredientLi = document.createElement("li")
-
-                ingredientsUl.appendChild(ingredientLi)
-                ingredientLi.textContent = ingredientString
-                ingredientString++;
-
-            }
-            //adding name and ingredients to div
-            ingAndPicDiv.append(picAndNameDiv, ingredientsUl)
-
-            //make li elements for the steps
-            for (var i = 0; i < instructions.length; i++) {
-                var currentStep = i + 1;
-                // console.log(currentStep)
-                var instructionsString = instructions[i].step;
-                // console.log(instructionsString)
-                var stepLi = document.createElement("li")
-                var instructionLi = document.createElement("li")
-
-                stepsOl.appendChild(stepLi)
-                stepLi.textContent = currentStep
-                stepsOl.appendChild(instructionLi)
-                instructionLi.textContent = instructionsString
-                currentStep++;
-            }
-            currentDishId.appendChild(ingAndPicDiv)
-            currentDishId.appendChild(stepsOl)
->>>>>>> 7991e6ba237f79c10834c229241e8cbdf3bd3dcd
         })
 }
 
 //inserts recipe found into html page
 function insertRecipe(data) {
-    var dishName = data.recipes[0].title;                                                   //name of dish
-    var ingredientList = data.recipes[0].extendedIngredients;                               //array ingredients
-    var instructions = data.recipes[0].analyzedInstructions[0].steps;                       //array of ingredients
-    var dishImage = data.recipes[0].image                                                   //URL for image
+    var dishName = data.title;                                                   //name of dish
+    var ingredientList = data.extendedIngredients;                               //array ingredients
+    var instructions = data.analyzedInstructions[0].steps;                       //array of ingredients
+    var dishImage = data.image                                                   //URL for image
 
     //changes header of dish name ingredients and steps
     $("#dish-name").text(dishName);
@@ -123,7 +62,7 @@ function insertRecipe(data) {
 function searchByIngredient() {
     var userInput = $("#search-bar").val();
 
-    searchByIngredientURL = "https://api.spoonacular.com/recipes/autocomplete?query=" + userInput + "&numbers=5&apiKey=" + spoonacularKey;
+    var searchByIngredientURL = "https://api.spoonacular.com/recipes/autocomplete?query=" + userInput + "&numbers=5&apiKey=" + spoonacularKey;
 
     fetch(searchByIngredientURL)
         .then(function (response) {
@@ -131,27 +70,28 @@ function searchByIngredient() {
         })
         .then(function (data) {
             var searchData = [];
+            var recipeData = data;
+            console.log(recipeData);
             for (var i = 0; i < data.length; i++) {
                 searchData[i] = data[i].title
             }
             $("#search-bar").autocomplete({
-                source: searchData
-            })
-                .then(function (data) {
-                    var searchData = [];
-                    for (var i = 0; i < data.length; i++) {
-                        searchData[i] = data[i].title
+                source: searchData,
+                select: function(evenet, ui){
+                    var selection = event.target.innerText;
+                    for(var i = 0; i < searchData.length; i++){
+                        if(selection == recipeData[i].title){
+                            var recipeId = recipeData[i].id;
+                            findRecipe(recipeId);
+                            break;
+                        }
                     }
-                    console.log(searchData);
-                    $("#searchBar").autocomplete({
-                        source: searchData
-                    })
-                })
+                }
+            })
         })
-    }
+}
 
-<<<<<<< HEAD
-function searchedRecipe(event){
+function searchedRecipe(event) {
     var userInput = event.target.innerText;
     console.log(userInput);
 }
@@ -165,7 +105,7 @@ function randomDrink() {
             "x-rapidapi-key": "74f2d339b2msh8cf0e09097065dep10d3f8jsne564c60a5f87"
         }
     })
-        .then(function(response) {
+        .then(function (response) {
             return response.json();
         })
         .then(function (data) {
@@ -195,10 +135,6 @@ function removeCurrentDrink() {
     $("#drink-ingredients").remove();
 }
 
-$("#searchBtn").click(function () {
-    searchByIngredient();
-})
-
 $("#generateRandomRecipe").click(function (event) {
     removeCurrentRecipe();
     randomRecipe();
@@ -207,102 +143,8 @@ $("#generateRandomRecipe").click(function (event) {
 $("#search-bar").keydown(function () {
     searchByIngredient();
 })
-=======
-    // Search random Cocktails
-    function randomDrink() {
-        fetch("https://cocktails3.p.rapidapi.com/random", {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-host": "cocktails3.p.rapidapi.com",
-                "x-rapidapi-key": ""
-            }
-        })
-            .then(response => {
-                console.log(response);
-                return response.json();
-            })
-            .then(function (data) {
-                console.log(data.body[0]);
-                var drinkName = data.body[0].name;
-                var drinkIngredients = data.body[0].ingredients;
-                console.log(data.body[0].ingredients);
 
-                //Change drink name and creates sections like ingredients
-                $("#currentDrink").append("<h3>" + drinkName + "</h2> <ul id=\"drinkIngredients\"></ul>");
-                $("#drinkIngredients").append("<h2>Ingredients</h2>");
-            });
-    };
-    // // Search random Cocktails
-    // function randomDrink() {
-    //     fetch("https://cocktails3.p.rapidapi.com/random", {
-    //         "method": "GET",
-    //         "headers": {
-    //             "x-rapidapi-host": "cocktails3.p.rapidapi.com",
-    //             "x-rapidapi-key": ""
-    //         }
-    //     })
-
-    //     $("#generateRandomRecipe").click(function (event) {
-    //         removeCurrentRecipe();
-    //         randomRecipe();
-    //     })
-
-    //         //li elements for drinks
-    //         for(let i = 0; i < drinkIngredients.length; i++){
-    //             $("#drinkIngredients").append("<li>" + drinkIngredients[i] + "</li>")
-    //         }
-    //     };
-    // };
-
-    //removes current content of recipe
-    function removeCurrentRecipe() {
-        $("#dishName").remove();
-        $("#dishImage").remove();
-        $("#ingredients").remove();
-        $("#steps").remove();
-    }
-
-    //remove current drink
-    function removeCurrentDrink() {
-        $("#drinkName").remove();
-        $("#drinkIngredients").remove();
-    }
-
-    $("#searchBtn").click(function () {
-        searchByIngredient();
-    })
-
-    $("#generateRandomRecipe").click(function (event) {
-        removeCurrentRecipe();
-        randomRecipe();
-    })
-
-    $("#search-bar").keydown(function () {
-        searchByIngredient();
-    })
->>>>>>> 7991e6ba237f79c10834c229241e8cbdf3bd3dcd
-
-$("ul").click(function(event){
-    console.log(event.target.innerText);
-    var userInput = event.target.innerText;
-    console.log(userInput);
-    searchedRecipe();
-})
-
-<<<<<<< HEAD
-$("#generateRandomDrink").click(function(event){
+$("#generateRandomDrink").click(function (event) {
     removeCurrentDrink();
     randomDrink();
-
 })
-=======
-    $("#generateRandomDrink").click(function (event) {
-        removeCurrentDrink();
-        randomDrink();
-    })
->>>>>>> 7991e6ba237f79c10834c229241e8cbdf3bd3dcd
-
-// $("body").click(function(event){
-//     console.log(event.target);
-//     console.log(event.target.innerText);
-// })
